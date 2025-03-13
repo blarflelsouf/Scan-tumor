e691c648e9e68d3ee2f185ed00656450f2ba8d8c
louis faye <louis.buray54@gmail.com>
2025-03-13 17:59:50 +0100

    vpropre

---
+++ scantumor/interface/main.py
@@ -0,0 +1,56 @@
+from ml_logic import preprocessor as prepro
+from ml_logic import modelVGG as modelvgg
+import utils
+
+
+
+path_train = '/home/blqrf/code/blarflelsouf/Scan-tumor/notebooks/Training'
+path_test = '/home/blqrf/code/blarflelsouf/Scan-tumor/notebooks/Testing'
+path_train_prepro = '/home/blqrf/code/blarflelsouf/Scan-tumor/notebooks/training_prepro'
+path_test_prepro = '/home/blqrf/code/blarflelsouf/Scan-tumor/notebooks/testing_prepro'
+
+
+
+
+
+
+
+                        ######### Preprocess data #########
+
+'''Preprocess the data'''
+
+### Variable for preprocessing data ###
+
+img_size = (150, 150)
+df_train = utils.load_data_dataframe(path_train)
+df_test = utils.load_data_dataframe(path_test)
+
+
+### Prepropress the data by resizing, padding ###
+
+# Stock the data preprocess in a directory data_train_prepro
+prepro.preprocess_write_squared_image_to_dir(df_train, img_size, root_dest_dir= path_train_prepro)
+
+# Stock the data preprocess in a directory data_test_prepro
+prepro.preprocess_write_squared_image_to_dir(df_test, img_size, root_dest_dir= path_test_prepro)
+
+
+
+
+
+### Variable for loading data ###
+
+batch_size_train = 512 # -> batch size of training model (64/128 recommended)
+patience = 1 # -> patience of early stopping
+epochs = 5 # -> number of epochs
+model = 'VGG' # -> 'CNN'//'VGG'//'ALL'
+
+
+### Train model ###
+
+
+if model == 'VGG':
+    history = modelvgg.train_model(path_train_prepro, path_test_prepro, epochs, patience, batch_size_train, img_size) #VGG16#
+
+
+print(history)
---
+++ scantumor/ml_logic/modelVGG.py
@@ -0,0 +1,102 @@
+from tensorflow.keras.applications.vgg16 import VGG16
+def load_model():
+    model = VGG16(weights="imagenet", include_top=False, input_shape=(150,150,3))
+    return model
+def set_nontrainable_layers(model):
+    model.trainable = False
+    return model
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras import layers, optimizers, callbacks, models
+from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.callbacks import EarlyStopping
+
+def add_last_layers(model):
+    '''Take a pre-trained model, set its parameters as non-trainable, and add additional trainable layers on top'''
+    non_trainable_model = set_nontrainable_layers(model)
+
+
+    # Convolutional Layers
+
+    new_model = Sequential()
+    new_model.add(non_trainable_model)
+    new_model.add(layers.Input((150, 150, 3)))
+    new_model.add(layers.Rescaling(1./255))
+    new_model.add(layers.Conv2D(filters=32, kernel_size=(3,3), activation="relu", padding="same"))
+    new_model.add(layers.MaxPooling2D(pool_size=(2, 2), padding="same") )
+
+    new_model.add(layers.Conv2D(filters=32, kernel_size=(3,3), activation="relu", padding="same"))
+    new_model.add(layers.MaxPooling2D(pool_size=(2, 2), padding="same") )
+
+    new_model.add(layers.Conv2D(filters=64, kernel_size=(3,3), activation="relu", padding="same"))
+    new_model.add(layers.MaxPooling2D(pool_size=(2, 2), padding="same") )
+
+    new_model.add(layers.Conv2D(filters=128, kernel_size=(3,3), activation="relu", padding="same"))
+    new_model.add(layers.MaxPooling2D(pool_size=(2, 2), padding="same") )
+
+    new_model.add(layers.Flatten())
+
+    new_model.add(layers.Dense(64, activation="relu"))
+
+    new_model.add(layers.Dropout(0.5))
+    new_model.add(layers.Dense(4, activation="softmax"))
+
+    return new_model
+
+def build_model():
+    model = load_model()
+    model = add_last_layers(model)
+
+    opt = optimizers.Adam(learning_rate=0.01)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=opt,
+                  metrics=['accuracy'])
+    return model
+
+def train_model(path_train_prepro, path_test_prepro, epochs, patience, batch_size, img_size):
+    print('🧮 Training of the model')
+    train_ds = image_dataset_from_directory(
+        path_train_prepro,
+        labels="inferred",
+        class_names=["notumor","glioma", 'meningioma', 'pituitary'],
+        label_mode="categorical",
+        seed=123,
+        validation_split=0.25,
+        subset='both',
+        image_size=img_size,
+        batch_size=batch_size)
+
+
+    test_ds = image_dataset_from_directory(
+        path_test_prepro,
+        labels="inferred",
+        class_names=["notumor","glioma", 'meningioma', 'pituitary'],
+        label_mode="categorical",
+        seed=123,
+        image_size=img_size,
+        batch_size=batch_size)
+
+
+    model=build_model()
+
+    es = EarlyStopping(
+        patience=patience,
+        restore_best_weights=True,
+        verbose=2
+    )
+
+
+    history = model.fit(
+            train_ds[0],
+            epochs=epochs,
+            callbacks=[es],
+            batch_size=batch_size,
+            validation_data=train_ds[1]
+            )
+
+    print('🧮 Predict of the test')
+    score = model.predict(test_ds)
+
+    print('⭐ Return of the results')
+
+    return history, score
